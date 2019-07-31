76eab68240b843922c50a14901f1578ce6dfe72f
MacBook <mr.kucher83@mail.ru>
2019-07-31 19:07:59 +0300

    Часть HBS Admin

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('admin');
+  res.render('admin/indexAdmin');
+})
+
+router.get('/list', function (req, res, next) {
+  res.render('admin/list');
+})
+
+router.get('/content', function (req, res, next) {
+  res.render('admin/content');
+})
+
+router.get('/test', function (req, res, next) {
+  res.render('admin/test');
+})
+
+router.get('/stats', function (req, res, next) {
+  res.render('admin/stats');
 })
 
 module.exports = router;
