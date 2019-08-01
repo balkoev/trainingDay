0e8ea163e9c2302908c87618e90498d70c3682f2
MacBook <mr.kucher83@mail.ru>
2019-08-01 10:20:04 +0300

    Подтянул модели ДБ

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const Cardbox = require('./../models/cardbox');
+const Card = require('./../models/card');
 
-router.get('/', (req, res, next) => {
-  res.render('admin/indexAdmin');
+
+const exposeTemplate = async function (req, res, next) {
+  const template = await hbs.getTemplate("views/admin/newCardbox.hbs", {
+      precompiled: true
+  });
+  const template2 = await hbs.getTemplate("views/admin/newCards.hbs", {
+      precompiled: true
+  });
+  res.newCardboxTemplate = template;
+  res.newCardsTemplate = template2;
+  next();
+}
+
+
+// ручки для перехода по основному меню!
+router.get('/', exposeTemplate, (req, res, next) => {
+  let cardbox = await Card.find();
+  res.render('admin/indexAdmin', {
+    cardbox,
+    template: res.newCardboxTemplate,
+    template2: res.newCardsTemplate
+  });
 })
 
 router.get('/list', function (req, res, next) {
@@ -20,5 +42,10 @@ router.get('/test', function (req, res, next) {
 router.get('/stats', function (req, res, next) {
   res.render('admin/stats');
 })
+// ----------------------------------------
+
+
+
+
 
 module.exports = router;
