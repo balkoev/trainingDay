877cf2f7c117d23e795fa5c2c3420e6857c1dbe8
balkoev <adam22185515@gmail.com>
2019-08-01 16:49:39 +0300

    SMS auth done

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('user/index');
+  res.render('user/indexUser');
 })
 
 module.exports = router;
