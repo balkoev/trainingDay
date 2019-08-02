c0dda0b653c7e1a643c4f57341fb09f404033799
Nikita Kalinnikov <nikitakalinnikov@KALL.local>
2019-08-02 11:03:58 +0300

    f

---
+++ public/js/user/start_test.js
@@ -0,0 +1,7 @@
+document.addEventListener("click", async function (e) {
+    if(e.target.id == "start"){
+        // console.log("hi")
+        let testName = document.getElementById("testName")
+        
+    }
+})
